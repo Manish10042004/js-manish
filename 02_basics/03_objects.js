cd5607ebb8ac8141f54e4026a2b25ae3ccefe359
Manish10042004 <manishShrivas142@gmail.com>
2025-06-25 08:48:23 +0000

    object1

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -5,7 +5,35 @@
 
 // object literals
 
-const JsUser={
+
+const mySym=Symbol("key1")
+const JsUser= {
     name:"Manish",
-    age:
+    "full name":"Manish Shrivas",
+    [mySym]:"mykey1",
+    age:18,
+    location:"jaipur",
+    email:"manish@google.com",
+    isLoggedIn:false,
+    lastLogindaus:["Monday","Saturday"]
 }
+
+console.log(JsUser.email);
+console.log(JsUser["email"]);
+console.log(JsUser["full name"]);
+console.log(JsUser.mySym);
+console.log(typeof JsUser[mySym]);
+
+JsUser.email="manish@chatgpt.com"
+// objec.freeze(JsUser)
+JsUser.email="manish@microsoft.com"
+console.log(JsUser);
+
+JsUser.greeting=function(){
+    console.log("Hello JS  user");
+}
+JsUser.greetingTwo=function(){
+    console.log(`Hello JS  user,${this.name}`);
+}
+console.log(JsUser.greeting());
+console.log(JsUser.greetingTwo());
