afd1a424bad045f5446a12fa542124c985fa0988
Manish10042004 <manishShrivas142@gmail.com>
2025-06-28 18:15:26 +0000

    Scope

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -45,3 +45,36 @@ function loginUserMessage(username="sam"){
 }
 // console.log(loginUserMessage("Manish"))
 console.log(loginUserMessage("Manish"))
+
+
+function calculatePrice(val1,val2,...num1){
+    return num1
+}
+// console.log(calculatePrice(200,400,500,4000))
+
+
+
+const user={
+    usernaem:"manish",
+    prices:199
+}
+
+function handleObject(anyobject){
+    console.log(`Usernmae is ${anyobject.username} and price is ${anyobject.price}`);
+}
+
+ handleObject(user)
+
+// handleObject({
+//     username:"sam",
+//     price:399
+// })
+
+const myNewArray=[200,400,100,600]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+// console.log(returnSecondValue(myNewArray));
+
+console.log(returnSecondValue([200,400,100,600]));
