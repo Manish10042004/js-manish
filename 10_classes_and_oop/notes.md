379b532e7c048a31534c6f5515ec31a21a77f42e
Manish10042004 <manishShrivas142@gmail.com>
2025-07-27 11:02:46 +0000

    OPPS

---
+++ 10_classes_and_oop/notes.md
@@ -0,0 +1,24 @@
+# javascript and classes
+
+## OOP
+
+## Object
+- collection of properties and method
+-toLowerCase
+
+## why use OOP
+
+## parts od OOP
+Object literal
+
+- Constructor function
+- Prototypes
+- Classes
+- Insrances(new, this)
+
+
+## 4 pillars
+Abstraction
+Encapsulation
+Inheritance
+Polymorphism
---
+++ 10_classes_and_oop/oop.js
@@ -0,0 +1,35 @@
+const user={
+    username:"Manish",
+    loginCount:8,
+    signedIn:true,
+
+    getUserDetails:function(){
+        // console.log("Got user details from database");
+        // console.log(`Username:${this.username}`);
+        // console.log(this);     
+    }
+}
+// console.log(user.username);
+// console.log(user.getUserDetails());
+// console.log(this);
+
+
+
+
+function User(username, loginCount, isLoggedIn){
+    this.username=username;
+    this.loginCount=loginCount;
+    this.isLoggedIn=isLoggedIn;
+
+    this.greeting=function(){
+        console.log(`Welcome ${this.username}`);
+        
+    }
+
+    return this
+}
+
+const userOne=new User("Manish",12,true);
+const userTwo=new User("ChaiAurCode",11,false);
+console.log(userOne.constructor);
+// console.log(userTwo);
