a54efa8f4b46921aa2b12ad1dc9d8923e7804c25
Manish10042004 <manishShrivas142@gmail.com>
2025-08-01 12:31:30 +0000

    GetterAndSetter

---
+++ 10_classes_and_oop/getter_setter.js
@@ -0,0 +1,25 @@
+class User {
+    constructor(email,password) {
+        this.email=email
+        this.password=password
+    }
+
+    get email(){
+        return this._email.toUpperCase()
+    }
+    set email(value){
+        this._email = value
+    }
+
+    get password(){
+        return `${this._password}manish`
+    }
+    set password(value){
+        this._password= value
+    }
+}
+
+const manish = new User("m@manish.ai","abc")
+console.log(manish.password);
+console.log(manish.email
+);
---
+++ 10_classes_and_oop/object_get_set.js
@@ -0,0 +1,16 @@
+const user = {
+    _email: 'm@hc.com',
+    _password: "abc",
+
+
+    get email(){
+        return this._email.toUpperCase()
+    },
+    
+    set email(value){
+        this._email = value
+    }
+}
+
+const tea = Object.create(User)
+console.log(tea.email);
